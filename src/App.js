537e81a68c50ba0f2130a484ba2a298b62bba829
Kevin Lau <lau.kevt@gmail.com>
2021-06-01 23:44:56 -0700

    adds state and connection to backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [userState, setUserState] = useState({
+    user: null,
+    characters: { data: [] },
+  });
+  // user: { uid: "ABC123", name: "Lorem Ipsum", email: "a@a.com" }
+
+  useEffect(() => {
+    const getAppData = async () => {
+      try {
+        const characters = await axios.get(
+          "http://localhost:3001/api/characters"
+        );
+        setUserState((prevState) => ({
+          ...prevState,
+          characters,
+        }));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getAppData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {userState.characters.data.map((char, idx) => (
+        <article key={idx}>
+          <p>Character {idx + 1}</p>
+          <p>Name: {char.name}</p>
+          <p>Race: {char.race}</p>
+          <p>Class: {char.class}</p>
+        </article>
+      ))}
+
+    </>
   );
 }
 
